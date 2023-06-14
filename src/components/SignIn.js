1282bb2a4753f920de90789aed5cc25dd9006854
Vic <vicjs612@gmail.com>
2023-06-14 12:10:24 -0500

    make some changes of CSS

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -39,7 +39,7 @@ const SignIn = () => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 4000);
+    }, 6000);
   }, []);
 
   return (
@@ -56,112 +56,120 @@ const SignIn = () => {
             src={process.env.PUBLIC_URL + "/R_logo_color.png"}
             alt="Rlogo"
           />
+          <div className="txt1">
+            <h3> Reminder</h3>
+          </div>
+          <div className="txt2">
+            <h4> Make life easier!</h4>
+          </div>
         </div>
       ) : (
-        <div className="login-wrap">
-          <div className="login-html">
-            <input
-              id="tab-1"
-              type="radio"
-              name="tab"
-              className="sign-in"
-              defaultChecked
-            />
-            <label htmlFor="tab-1" className="tab">
-              Sign In
-            </label>
-            <input id="tab-2" type="radio" name="tab" className="sign-up" />
-            <label htmlFor="tab-2" className="tab">
-              Sign Up
-            </label>
-            <form className="login-form" onSubmit={login}>
-              <div className="sign-in-htm">
-                <div className="group">
-                  {/* <label htmlFor="user" className="label">
+        <div className="bkg">
+          <div className="login-wrap">
+            <div className="login-html">
+              <input
+                id="tab-1"
+                type="radio"
+                name="tab"
+                className="sign-in"
+                defaultChecked
+              />
+              <label htmlFor="tab-1" className="tab">
+                Sign In
+              </label>
+              <input id="tab-2" type="radio" name="tab" className="sign-up" />
+              <label htmlFor="tab-2" className="tab">
+                Sign Up
+              </label>
+              <form className="login-form" onSubmit={login}>
+                <div className="sign-in-htm">
+                  <div className="group">
+                    {/* <label htmlFor="user" className="label">
                     Username
                   </label> */}
-                  <input
-                    id="user"
-                    onChange={handleTextChange}
-                    value={state.username}
-                    name="username"
-                    label="Username"
-                    type="text"
-                    className="input"
-                    placeholder="Username"
-                  />
+                    <input
+                      id="user"
+                      onChange={handleTextChange}
+                      value={state.username}
+                      name="username"
+                      label="Username"
+                      type="text"
+                      className="input"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      id="pass"
+                      onChange={handleTextChange}
+                      value={state.password}
+                      name="password"
+                      type="password"
+                      className="input"
+                      data-type="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      id="check"
+                      type="checkbox"
+                      className="check"
+                      defaultChecked
+                    />
+                    <label htmlFor="check">
+                      <div className="hr"></div>
+                    </label>
+                  </div>
+                  <div className="group">
+                    <input type="submit" className="button" value="Sign In" />
+                  </div>
+                  <div className="foot-lnk">
+                    <a href="#forgot">Forgot Password?</a>
+                  </div>
                 </div>
-                <div className="group">
-                  <input
-                    id="pass"
-                    onChange={handleTextChange}
-                    value={state.password}
-                    name="password"
-                    type="password"
-                    className="input"
-                    data-type="password"
-                    placeholder="Password"
-                  />
+                <div className="sign-up-htm">
+                  <div className="group">
+                    <input
+                      id="user2"
+                      type="text"
+                      className="input"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      id="pass2"
+                      type="password"
+                      className="input"
+                      data-type="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      id="repeatpass"
+                      type="password"
+                      className="input"
+                      data-type="password"
+                      placeholder="Repeat Password"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      id="email"
+                      type="text"
+                      className="input"
+                      placeholder="E-mail"
+                    />
+                  </div>
+                  <div className="hr"></div>
+                  <div className="group">
+                    <input type="submit" className="button" value="Sign Up" />
+                  </div>
                 </div>
-                <div className="group">
-                  <input
-                    id="check"
-                    type="checkbox"
-                    className="check"
-                    defaultChecked
-                  />
-                  <label htmlFor="check">
-                    <div className="hr"></div>
-                  </label>
-                </div>
-                <div className="group">
-                  <input type="submit" className="button" value="Sign In" />
-                </div>
-                <div className="foot-lnk">
-                  <a href="#forgot">Forgot Password?</a>
-                </div>
-              </div>
-              <div className="sign-up-htm">
-                <div className="group">
-                  <input
-                    id="user2"
-                    type="text"
-                    className="input"
-                    placeholder="Username"
-                  />
-                </div>
-                <div className="group">
-                  <input
-                    id="pass2"
-                    type="password"
-                    className="input"
-                    data-type="password"
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="group">
-                  <input
-                    id="repeatpass"
-                    type="password"
-                    className="input"
-                    data-type="password"
-                    placeholder="Repeat Password"
-                  />
-                </div>
-                <div className="group">
-                  <input
-                    id="email"
-                    type="text"
-                    className="input"
-                    placeholder="E-mail"
-                  />
-                </div>
-                <div className="hr"></div>
-                <div className="group">
-                  <input type="submit" className="button" value="Sign Up" />
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       )}
